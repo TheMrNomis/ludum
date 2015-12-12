cd968abb74735de29e6b56e3026dfb87f43ee7bf
clem <clem.delambily@gmail.com>
2015-12-12 19:56:13 +0100

    add comment

--- Header/Building.h
+++ Header/Building.h
@@ -18,10 +18,33 @@ class Building : public INDrawable
 		const unsigned int m_currentBurnedDamage;
 
 	public:
+		 /**
+        	 * @brief constuctor for building
+        	 *
+        	 * @param textureLoader: the link of the texture file
+        	 * @param maxBurnedDamage: Number max of domage for building
+		 * @param currentBurnedDamage: actual Number of domage il the building
+        	 */
 		Building(TextureLoader const * textureLoader, unsigned int maxBurnedDamage = 0, unsigned int currentBurnedDamage = 0);
+
+		/**
+         	* @brief destructor for Canape
+         	*
+         	*/
 		virtual ~Building();
-	
+		
+		/**
+		*@brief show the building
+		*
+		*@param window: window whitch containt the building
+		*/
 		virtual void draw(sf::RenderWindow *window) const ;
+
+		/**
+		*@brief charge the level from files
+		*
+		*@param window: window whitch containt the building
+		*/
 		void loadToTileSet(std::string const & path);
 };
 
