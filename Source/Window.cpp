236cc802aacd588b49082eae6f9705ec81f18bc6
TheMrNomis <amaury.louarn@homnomnom.fr>
2015-12-12 18:56:42 +0100

    cleaned Window

--- Source/Window.cpp
+++ Source/Window.cpp
@@ -1,33 +1,49 @@
 #include "Window.h"
 #include "Floor.h"
 
-Window::Window(sf::RenderWindow * window) :m_window(window), m_currentWorld(new World())
+Window::Window(sf::RenderWindow * window):
+    m_window(window),
+    m_currentWorld(new World()),
+    m_currentStatus(GAME_MAIN_MENU)
 {
 }
 
 
 Window::~Window()
 {
+    delete m_currentWorld;
 }
 
-void Window::draw(sf::RenderWindow *window) const
+void Window::run()
 {
-	while (window->isOpen())
-	{
-		sf::Event event;
-		while (window->pollEvent(event))
-		{
-            switch(event.type)
-            {
-                case sf::Event::Closed:
-                    window->close();
-            }
-		}
-		window->clear();
-		
-		m_currentWorld->draw(window);
-	
+    while(m_currentStatus != GAME_STOPPED)
+    {
+        //events
+        sf::Event event;
+        while(m_window->pollEvent(event))
+            this->react(event);
+ 
+        //drawing
+        m_window->clear();
+        this->draw();
+        m_window->display();
+    }
+    m_window->close();
+}
 
-		window->display();
-	}
+void Window::draw() const
+{
+    m_currentWorld->draw(m_window);
+}
+
+void Window::react(sf::Event const& event)
+{
+    switch(event.type)
+    {
+        case sf::Event::Closed:
+            m_currentStatus = GAME_STOPPED;
+            break;
+        default:
+            break;
+    }
 }
