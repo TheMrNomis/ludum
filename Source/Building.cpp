344555e6bc2e4912a7945cacbc9aa14486f39a10
TheMrNomis <amaury.louarn@homnomnom.fr>
2015-12-13 01:00:51 +0100

    removed useless code

--- Source/Building.cpp
+++ Source/Building.cpp
@@ -44,56 +44,15 @@ void Building::loadToTileSet(std::string const &path)
                 m_floors.push_back(currentFloor);
                 currentFloor = new Floor(m_textureLoader->getFloorTexture());
             }
-            std::vector<unsigned char> buffer;
-            for(auto it = line.cbegin(); it != line.cend(); ++it)
-                buffer.push_back(*it);
+            else
+            {
+                std::vector<unsigned char> buffer;
+                for(auto it = line.cbegin(); it != line.cend(); ++it)
+                    buffer.push_back(*it);
 
-            currentFloor->addLine(buffer);
+                currentFloor->addLine(buffer);
+            }
         }
     }
     m_floors.push_back(currentFloor);
-
-	/*
-    std::ifstream myfile(path, std::ios_base::in);
-	std::vector<unsigned char> buffer;
-
-	char currentChar = ' ';
-	unsigned int currentId;
-	int currentID = 0;
-	int currendFloor = 0;
-	std::string test;
-
-	std::string tailleBuffer;
-	while(myfile >> currentChar)
-	{
-		while (currentChar != '=')
-			tailleBuffer += myfile.get();
-		break;
-	}
-    */
-
-	//this->setLargeurBuilding(std::stoi(tailleBuffer));
-
-	//std::cout << test;
-
-
-
-	//while(myfile >> currentChar)
-	//{
-	//	
-	//	
-	//	if(currentChar == '=')
-	//	{
-	//		std::cout << "endFloor : " << currendFloor << std::endl;
-	//		currendFloor++;
-
-	//		m_floors.push_back(new Floor(new std::vector<unsigned char>(buffer),m_textureLoader->getFloorTexture()));
-	//		buffer.clear();
-
-	//		currentID = 0;
-	//	}
-
-	//	else
-	//		buffer.push_back(currentChar);
-	//}
 }
