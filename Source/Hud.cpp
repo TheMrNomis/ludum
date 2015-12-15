751ba70edb042129f39db1378e46dd05a3f90783
TheMrNomis <n0m1s@homnomnom.fr>
2015-12-15 01:25:07 +0100

    ajout d'une texture de hachure pour l'eau

--- Source/Hud.cpp
+++ Source/Hud.cpp
@@ -10,7 +10,6 @@ void Hud::draw(sf::RenderWindow * window) const
     sf::View view = window->getDefaultView();
     view.setViewport(sf::FloatRect(0.66, 0, 0.34, 0.1));
     window->setView(view);
-
 }
 
 void Hud::update(sf::Clock const& clk)
